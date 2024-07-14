fa0c3e4a489a891f6fd0eb83e8b786014eb0e3b2
Brandon-Vetter <brandon.w.vetter@gmail.com>
2024-07-13 18:21:20 -0600

    added comments to functions

--- module_extractor.py
+++ module_extractor.py
@@ -1,7 +1,36 @@
+#!/usr/lib/python3
+
 import os
 import argparse
 
+"""
+Author: Brandon Vetter
+Description: 
+takes verilog files, finds the modules and extracts information
+from them.  It will output the modules in a component format, or
+can returns the names of the modules.
+Paramters:
+filename - the file to extract module information from
+
+-f, --output_file_location - location to output file to.  If left with
+just the flag, output will go to the file where the module was extracted
+from.  If file exists will append to file.
+
+-q, --quiet_output - stops output from being printed from stdout.
+
+-n, --print_names - prints only the names of the modules
+
+-s, --specify_modules - only get the data from the specific modules listed
+
+-na, --not_append - will overwrite a file instead of appending to it
+"""
 class ModuleDataValues:
+    """
+    Is a basic class to store data of the parameters.
+    Is basicly just a struct with the added feature of
+    being able to print its data as a string.
+    """
+
     def __init__(self, data_type, data_width='1 bit', bus_width=''):
         self.data_type=data_type
         self.data_width=data_width
@@ -23,6 +52,11 @@ class ModuleDataValues:
         return output_str
 
 def main():
+    """
+    Is the main function ran.
+    Runs the parser and runs the specified files.
+    """
+
     parser = argparse.ArgumentParser(
         prog='Verilog module extractor',
         description="Searches for modules in a .sv file and outputs a copiable version of the components to use elsewhere in the project.",
@@ -61,7 +95,7 @@ def main():
 
     if not args.quiet_stdout:
         for data in get_data:
-            print(data + "\n")
+            print(data)
     
     if args.output_file_location != None:
         output_file = args.output_file_location
@@ -71,15 +105,20 @@ def main():
         IO_file = None
         if os.path.isfile(output_file) and not args.not_append:
             IO_file = open(output_file, 'a')
+            IO_file.write("\n\n")
         else:
             IO_file = open(output_file, 'w')
         
         for data in get_data:
-            IO_file.write("\n\n"+data+"\n\n")
+            IO_file.write(data)
+        IO_file.close()
 
 
 
 def parse_modules(file_name):
+    """
+    Parses out the modules of the file
+    """
     file = open(file_name, 'r')
     current_line = 0
     modules = []
@@ -98,6 +137,10 @@ def parse_modules(file_name):
     return modules
 
 def extract_modules(file_name, modules_to_extract = []):
+    """
+    Extracts out the paramters of the modules and returns the data
+    in an array of strings consisting of a module that can be used.
+    """
     modules = parse_modules(file_name)
     inputs = []
     outputs = []
@@ -109,6 +152,8 @@ def extract_modules(file_name, modules_to_extract = []):
             continue
 
         wire_inputs = 2
+
+        # checks for if parameters exist and parse their data
         if "#" in module[2]:
             wire_inputs = 4
             static_parameters = module[3].replace('(', '').replace(')', '')
@@ -134,6 +179,8 @@ def extract_modules(file_name, modules_to_extract = []):
         parsed_parameters = parse_line(parameters, split_char=',', ignore_escape=True)
         ip = False
         op = False
+        
+        #parse module wire parameters
         for parameter in parsed_parameters:
             if 'input' in parameter or 'output' in parameter:
                 data_value = parse_line(parameter)
@@ -162,6 +209,7 @@ def extract_modules(file_name, modules_to_extract = []):
                 elif op:
                     outputs[-1].values.append(parameter.strip())
         
+        # add the names for the sections
         print_str = name
         if static_data != []:
             print_str += " #(\n"
@@ -178,7 +226,9 @@ def extract_modules(file_name, modules_to_extract = []):
         print_str += "// OUTPUTS\n"
         for output in outputs:
             print_str += output.output_data_string()
-        return_data.append(print_str + ');')
+        
+        # add to list
+        return_data.append(print_str + ");\n")
     
     return return_data
                 
@@ -187,6 +237,10 @@ def extract_modules(file_name, modules_to_extract = []):
 # need to write parser
 
 def read_line(file):
+    """
+    Reads the lines of the file and removes the comments.
+    splits the lines by ";".
+    """
     line = ''
     single_line_comment = False
     multi_line_comment = True
@@ -213,6 +267,9 @@ def read_line(file):
     return line.strip()
 
 def get_module_names(file_name):
+    """
+    Returns the names of the modules of the file
+    """
     modules = parse_modules(file_name)
     names = []
     for module in modules:
@@ -221,6 +278,9 @@ def get_module_names(file_name):
     return names
 
 def parse_line(line, split_char = ' ', special_chars = [], ignore_escape=False):
+    """
+    Returns a parsed array from a line.
+    Used to parse the module lines."""
     term_list = []
     current_term = ''
     escape_term = ''
@@ -238,6 +298,7 @@ def parse_line(line, split_char = ' ', special_chars = [], ignore_escape=False):
     
 
     for char in line:
+        # if (), [], {} found, include spaces
         if char in escape_table.keys() and not ignore_escape:
             if nested_type == '':
                 nested_type = char
@@ -250,7 +311,7 @@ def parse_line(line, split_char = ' ', special_chars = [], ignore_escape=False):
             elif nested_type == char:
                 nested_count += 1
 
-
+        # include spaces, unless end of (), [], {}
         if include_spaces:
             current_term += char
             if char == escape_term and nested_count == 0:
@@ -273,7 +334,6 @@ def parse_line(line, split_char = ' ', special_chars = [], ignore_escape=False):
                         term_list.append(spe_char)
                         current_term = ''
 
-                
         elif current_term != '':
             term_list.append(current_term)
             current_term = ''
